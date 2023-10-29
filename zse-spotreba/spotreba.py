74bf011a948d3e314a8a9ccfa5732ceba7d17cc7
Stanley <stanoba@gmail.com>
2023-10-29 08:16:45 +0100

    Add tabulate to format table

--- zse-spotreba/spotreba.py
+++ zse-spotreba/spotreba.py
@@ -1,8 +1,10 @@
 # pip3 install pandas
-# pip install xlrd
+# pip3 install xlrd
+# pip3 install tabulate
 
 import pandas as pd
 import math
+from tabulate import tabulate, SEPARATING_LINE
 
 ##################################### Tariffs ####################################
 # stav ku 31.8.2023
@@ -37,9 +39,10 @@ df2 = pd.DataFrame([
 [date_from, date_to, days,"Cena za elektrinu", total_consumption, "kWh", kwh, (kwh*total_consumption)],
 [date_from, date_to, days,"Mesačná platba za jedno odberné miesto", "1.00", monthly_pay, "1 M",monthly_pay],
 [date_from, date_to, days, "Spotrebná daň", total_consumption, "kWh", spotr_dan, (spotr_dan*total_consumption)],
+SEPARATING_LINE,
 ["Spolu bez DPH","","","","","","",str(round(ZSD,2))+" €"]
 ],
-columns=['Dátum od', 'Dátum do', 'Dni', 'Názov položky', 'Množstvo', 'Cena bez DPH (€/mer.j.)', 'Obdobie pre cenu', 'Suma bez DPH (€)'])
+columns=['Dátum od', 'Dátum do', 'Dni', 'Názov položky', 'Množstvo', 'Cena bez DPH\n (€/mer.j.)', 'Obdobie\npre cenu', 'Suma bez\nDPH (€)'])
 
 df3 = pd.DataFrame([
 [date_from, date_to, days,"Platba za systémové služby", total_consumption, "kWh", sys_sluzby, round((sys_sluzby*total_consumption),2)],
@@ -48,16 +51,23 @@ df3 = pd.DataFrame([
 [date_from, date_to, days, "Variabilná zložka tarify za distribúciu", total_consumption, "kWh", var_zlozka, round((var_zlozka*total_consumption),2)],
 [date_from, date_to, days, "Platba za straty elektr.pri distr.el.", total_consumption, "kWh", straty, round((straty*total_consumption),2)],
 [date_from, date_to, days, "Odvod do jadrového fondu", total_consumption, "kWh", jadr_fond, round((jadr_fond*total_consumption),2)],
+SEPARATING_LINE,
 ["Spolu bez DPH","","","","","","",str(round(ZSE,2))+" €"]
 ],
-columns=['Dátum od', 'Dátum do', 'Dni', 'Názov položky', 'Množstvo', 'Cena bez DPH (€/mer.j.)', 'Obdobie pre cenu', 'Suma bez DPH (€)'])
+columns=['Dátum od', 'Dátum do', 'Dni', 'Názov položky', 'Množstvo', 'Cena bez DPH\n (€/mer.j.)', 'Obdobie\npre cenu', 'Suma bez\nDPH (€)'])
 
 ##########################################################################################################################################
+print()
 print("Dodávka elektriny: produkt DomovKlasik - DD2 (fakturačné položky)")
-print(df2)
 
+print(tabulate(df2, headers = 'keys', tablefmt = 'psql', showindex="False"))
+#print(df2)
+
+print()
 print("Distribúcia a regulované poplatky: produkt D2 (fakturačné položky)")
-print(df3)
+#print(df3)
+print(tabulate(df3, headers = 'keys', tablefmt = 'psql', showindex="False"))
 
 print("Spolu celkovo bez DPH",round(ZSD + ZSE,2),"€")
-print("Spolu celkovo bez DPH",round((ZSD + ZSE)*1.2,2),"€")
+print("Spolu celkovo s DPH",round((ZSD + ZSE)*1.2,2),"€")
+print()
